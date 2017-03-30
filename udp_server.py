e5eb56cca3d2861c1ea2e6517ff6d0f7922fb858
Thierry Frycia <thierry@frycia.be>
2017-03-30 18:35:41 +0200

    Add /pseudo command

--- udp_server.py
+++ udp_server.py
@@ -8,7 +8,8 @@ class Server:
         self._host = host
         self._port = port
         self._clients = []
-        self._addr = ""
+        self._addr = ''
+        self._pseudo = ''
 
         # UDP socket
         try :
@@ -41,10 +42,20 @@ class Server:
             
             
             if data.decode() == '/join':
-                self._join()
+                if self._pseudo == '':
+                    self._s.sendto('Veuillez définir vortre pseudo avec /pseudo <nom> avant de rejoindre le serveur'.encode(), self._addr)
+                else:
+                    self._join()
             elif data.decode() == '/clients':
                 message = str(self._clients) + '\n'
                 self._s.sendto(message.encode(), self._addr)
+            elif '/pseudo' in data.decode():
+                try:
+                    self._pseudo = data.decode().split()[1]
+                except:
+                    self._s.sendto('Veuillez définir vortre pseudo avec /pseudo <nom>.'.encode(), self._addr)
+                confirm = 'Votre pseudo, ' + self._pseudo + ', a bien été enregistré.'
+                self._s.sendto(confirm.encode(), self._addr)
             else:
                 self._s.sendto(err_msg.encode(), self._addr)
 
@@ -52,10 +63,10 @@ class Server:
             print('Message[' + self._addr[0] + ':' + str(self._addr[1]) + '] - ' + data.decode().strip())
 
     def _join(self):
-        if self._addr in self._clients:
+        if self._pseudo in self._clients:
             reply = 'Vous êtes déjà dans la liste des clients disponibles\nÉcrivez /clients pour voir la liste des clients disponibles\n' 
         else:
-            self._clients.append(self._addr)
+            self._clients.append(self._pseudo)
             reply = 'Vous avez été ajouté à la liste des clients disponibles\nÉcrivez /clients pour voir la liste des clients disponibles\n'
 
         self._s.sendto(reply.encode(), self._addr)
