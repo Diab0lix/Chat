e5bd477839eb623fb2649817a85f2dea5fcc2237
Thierry Frycia <thierry@frycia.be>
2017-03-30 15:57:59 +0200

    renamed file

--- udp_server.py
+++ udp_server.py
@@ -1,61 +1,71 @@
 import socket
 import sys
 
-HOST = ''    # Symbolic name meaning all available interfaces
-PORT = 8888    # Arbitrary non-privileged port
 
-# Datagram (udp) socket
-try :
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    print('Socket created')
-except (socket.error, msg):
-    print('Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-    sys.exit()
+class Server:
+    def __init__(self, port=8888, host=''):
+        
+        self._host = host
+        self._port = port
+        self._clients = []
+        self._addr = ""
 
-
-# Bind socket to local host and port
-try:
-    s.bind((HOST, PORT))
-except (socket.error, msg):
-    print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-    sys.exit()
-
-print('Socket bind complete')
-print('listening on ' + HOST + ':' + str(PORT))
+        # UDP socket
+        try :
+            self._s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            print('Socket created')
+        except (socket.error, msg):
+            print('Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+            sys.exit()
 
 
-clients = []
+        # Bind socket to local host and port
+        self._s.bind((self._host, self._port))
 
-def join():
-    if addr in clients:
-        reply = 'Vous êtes déjà dans la liste des clients disponibles\nÉcrivez /clients pour voir la liste des clients disponibles\n' 
-    else:
-        clients.append(addr)
-        reply = 'Vous avez été ajouté à la liste des clients disponibles\nÉcrivez /clients pour voir la liste des clients disponibles\n'
+        print('Socket bind complete')
+        print('listening on ' + self._host + ':' + str(self._port))
 
-    s.sendto(reply.encode(), addr)
+    def run(self):
+        # now keep talking with the client
+        while True:
+            # receive data from client (data, addr)
+            d = self._s.recvfrom(1024)
+            data = d[0]
+            self._addr = d[1]
 
-# now keep talking with the client
-while True:
-    # receive data from client (data, addr)
-    d = s.recvfrom(1024)
-    data = d[0]
-    addr = d[1]
+            # don't continue if we didn't receive anything
+            if not data:
+                break
 
-    # don't continue if we didn't receive anything
-    if not data:
-        break
+            err_msg = 'Commande inconnue, écrivez /join pour vous ajouter à la liste des clients disponibles ou /clients pour consulter cette liste.\n'
+            
+            
+            if data.decode() == '/join':
+                self._join()
+            elif data.decode() == '/clients':
+                message = str(self._clients) + '\n'
+                self._s.sendto(message.encode(), self._addr)
+            else:
+                self._s.sendto(err_msg.encode(), self._addr)
 
-    err_msg = 'Commande inconnue, écrivez /join pour vous ajouter à la liste des clients disponibles ou /clients pour consulter cette liste.\n'
-    
-    
-    if data.decode() == '/join':
-        join()
-    elif data.decode() == '/clients':
-        s.sendto(str(clients).encode(), addr)
-        s.sendto('\n'.encode(), addr)
-    else:
-        s.sendto(err_msg.encode(), addr)
+            # log all messages
+            print('Message[' + self._addr[0] + ':' + str(self._addr[1]) + '] - ' + data.decode().strip())
 
-    # log all messages
-    print('Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + data.decode().strip())
+    def _join(self):
+        if self._addr in self._clients:
+            reply = 'Vous êtes déjà dans la liste des clients disponibles\nÉcrivez /clients pour voir la liste des clients disponibles\n' 
+        else:
+            self._clients.append(self._addr)
+            reply = 'Vous avez été ajouté à la liste des clients disponibles\nÉcrivez /clients pour voir la liste des clients disponibles\n'
+
+        self._s.sendto(reply.encode(), self._addr)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and type(sys.argv[1]):
+        try:
+            Server(int(sys.argv[1])).run()
+        except:
+            print('Veuillez entrer un nombre de port')
+    else: 
+        Server().run()
