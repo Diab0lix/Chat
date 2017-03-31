84de509f12e3bf26c99ee2b8f1fb0e5dc3540217
Thierry Frycia <thierry@frycia.be>
2017-03-31 13:00:25 +0200

    finalised client

--- udp_client.py
+++ udp_client.py
@@ -1,31 +1,39 @@
 import socket   #for sockets
 import sys      #for exit
 
-# create dgram udp socket
-try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-except socket.error:
-        print('Failed to create socket')
-        sys.exit()
-
-host = 'localhost';
-port = 8888;
-
-while True:
-    msg = input().encode()
+class Client:
+    def __init__(self, port=8888, host='localhost'):
         
-#        try :
-    #Set the whole string
-    s.sendto(msg, (host, port))
-            
-    # receive data from client (data, addr)
-    d = s.recvfrom(1024)
-    reply = d[0]
-    addr = d[1]
-            
-    print(reply.decode())
-        
- #       except (socket.error, msg):
- #               print('Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
- #               sys.exit()
+        self._host = host
+        self._port = port
+
+        # UDP socket
+        try:
+                self._s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        except socket.error:
+                print('Failed to create socket')
+                sys.exit()
+
+    def run(self):
+        while True:
+            msg = input().encode()
+                
+            #Set the whole string
+            self._s.sendto(msg, (self._host, self._port))
+                    
+            # receive data from client (data, addr)
+            d = self._s.recvfrom(1024)
+            reply = d[0]
+            addr = d[1]
+                    
+            print(reply.decode())
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and type(sys.argv[1]):
+        try:
+            Client(int(sys.argv[1])).run()
+        except:
+            print('Please enter a port number')
+    else: 
+        Client().run()
 
